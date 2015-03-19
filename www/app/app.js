716e21697e0b725eeb11f2a4b5da21c2ff9f366a
Listbd <Listbd@gmail.com>
2015-03-19 04:00:26 -0700

    Step 1

---
+++ www/app/app.js
@@ -0,0 +1,37 @@
+﻿angular.module("eliteApp", ["ionic"])
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            corddova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+
+    });
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            abstract: true,
+            url: "/home",
+            templateUrl: "app/home/home.html"
+        })
+        .state('home.leagues', {
+            url: "/leagues",
+            views: {
+                "tab-leagues": {
+                    templateUrl: "app/home/leagues.html"
+                }
+            }
+        })
+        .state('home.myteams', {
+            url: "/myteams",
+            views: {
+                "tab-myteams": {
+                    templateUrl: "app/home/myteams.html"
+                }
+            }
+        });
+    $urlRouterProvider.otherwise('/home/leagues');
+});
